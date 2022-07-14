02807000891f57ae3bf830f2b5757748e64b4cb7
Артём <artem2004920@gmail.com>
2022-07-14 19:17:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+first_command = str(input('Введите "да", если хотите ебаться: '))
+second_command = str(input('Введите "нет", если не хотите ебаться: '))
+if first_command.lower() == 'да':
+    print('Поздравляю, вы животное!')
+elif second_command.lower != 'нет':
+    print('Поздравляю, вы животное!')
+else:
+    print('Вы ебаный')
